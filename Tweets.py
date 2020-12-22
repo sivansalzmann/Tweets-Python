492b3b801880492a36d55fb89fceafe274e3cce9
sivansalzmann <sivansalzmann10@gmail.com>
2020-12-22 17:41:07 +0200

    hedaers functions

--- Tweets.py
+++ Tweets.py
@@ -4,3 +4,71 @@ import numpy as np
 import pandas as pd
 import csv
 
+class Tweet:
+    id = 0
+    user = 0
+    fullname = 0
+    url = 0
+    timestemp = 0
+    replies = 0
+    likes = 0
+    retweets = 0
+    text = 0
+
+    def __init__(self):
+        #constractur
+
+    def setTweets(self):
+
+    def getId(self):
+
+    def getUser(self):
+
+    def getFullname(self):
+
+    def getUrl(self):
+
+    def getTimestemp(self):
+
+    def getReplies(self):
+
+    def getLikes(self):
+
+    def getRetweets(self):
+
+    def getText(self):
+
+    #first task
+    def textOfMonth(self):
+
+    def findSigns(self,sign): #uses for hashtag and website
+
+    def hashtagValidation(self):
+
+    def mostUsedHahtags(self): #dont forget add symbol
+
+    #second task
+    def usersOfMonth(self):
+
+    def usernameValisation(self):
+
+    def mostMentionUsername(self): #dont forget add symbol
+
+    #third task
+    def websiteOfMonth(self):
+
+    def websiteValisation(self):
+
+    def mostMentionWebsite(self): #dont forget remove things
+
+    def dateFormat(self): #help to save to csv just month wnd year
+
+    def checkRowValidation(self): # put value None
+
+    def saveToCsv(self): #save res to csv
+
+
+
+
+
+
